0837a427123ea996c115e25014ba5b52b40c8ffb
gorash-bolatu <cotwi10@yandex.ru>
2025-05-14 05:39:37 +0800

    for test 1

---
+++ Station.java
@@ -0,0 +1,42 @@
+public class Station {
+
+    String line;
+    String name;
+    boolean isAvailable;
+    Station prev, next;
+
+    public Station(String line, String name) {
+        this.line = line;
+        this.name = name;
+    }
+
+    public void switchAvailable() {
+        this.isAvailable = !this.isAvailable;
+    }
+
+    public void addNext(Station station) {
+        this.next = station;
+    }
+
+    public void addPrev(Station station) {
+        this.prev = station;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(this.getClass().getName().toUpperCase());
+        s.append(" ");
+        s.append(this.name);
+        s.append(": ");
+        s.append(this.line);
+        s.append(" line, in service: ");
+        s.append(this.isAvailable);
+        s.append(", previous station: ");
+        s.append(prev.name);
+        s.append(", next station: ");
+        s.append(next.name);
+        return s.toString();
+    }
+
+}
