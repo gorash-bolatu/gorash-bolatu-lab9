80857d06eb2e8a8ee08e197c826a051cffa3a9c0
gorash-bolatu <cotwi10@yandex.ru>
2025-05-14 05:56:14 +0800

    for test 6

--- TransferStation.java
+++ TransferStation.java
@@ -1,14 +1,32 @@
+
+import java.util.ArrayList;
+
 public class TransferStation extends Station {
+
+    private final ArrayList<Station> transfers;
 
     public TransferStation(String line, String name) {
         super(line, name);
+        this.transfers = new ArrayList<>();
     }
 
+    public void addTransferStationNext(Station station) {
+        station.prev = this;
+        transfers.add(station);
+    }
+    
+    public void addTransferStationPrev(Station station) {
+        station.next = this;
+        transfers.add(station);
+    }
     @Override
     public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(super.toString());
+        StringBuilder s = new StringBuilder(super.toString());
         s.append("\n\tTransfers: \n");
+        for (Station station : transfers) {
+            s.append(station.toString());
+            s.append("\n");
+        }
         return s.toString();
     }
 
